87298b486068e9267ec43b76d6c8f2c9e77f9394
Casey K <tehcnicolour@gmail.com>
2022-01-19 14:23:26 -0800

    rm incorrect logo path

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ module.exports = {
   siteHeadline: 'A digital studio for the decentralized web.', // Headline for schema.org JSONLD
   siteUrl: 'http://tetrashapes.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en-US', // Language Tag on <html> element
-  siteLogo: 'static/favicon.svg', // Used for SEO and manifest
+  siteLogo: '', // Used for SEO and manifest
   siteDescription: 'Tetra / A digital studio for the decentralized web. We build decentralized web and native apps for web 3.0.',
 
   author: 'Tetra', // Author for schema.org JSONLD
